dd6a7864ce0ad3bebfd3fb911da22e86fcbf4491
PaulTamayoS <70984734+PaulTamayoS@users.noreply.github.com>
2026-05-04 18:21:44 +0200

    uncommented the development fallback, and added a ExecutorService to avoid using the UI thread

--- app/src/main/java/com/games4science/truerandommusicplayer/model/AppDatabase.java
+++ app/src/main/java/com/games4science/truerandommusicplayer/model/AppDatabase.java
@@ -4,8 +4,12 @@ import android.content.Context;
 import androidx.room.Database;
 import androidx.room.Room;
 import androidx.room.RoomDatabase;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 import com.games4science.truerandommusicplayer.util.MyConstants;
+
+import java.util.concurrent.ExecutorService;
 
 // We list all 3 entities and set the version to 1
 @Database(entities = {Track.class, Playlist.class, JoinPlaylistTrack.class}, version = 1, exportSchema = false)
@@ -14,13 +18,16 @@ public abstract class AppDatabase extends RoomDatabase {
     public abstract LibraryDao libraryDao();
     private static volatile AppDatabase INSTANCE;
 
+    private static final int NUMBER_OF_THREADS = 4;
+    public static final ExecutorService databaseWriteExecutor = Executors.newFixedThreadPool(NUMBER_OF_THREADS);
+
     public static AppDatabase getDatabase(final Context context) {
         if (INSTANCE == null) {
             synchronized (AppDatabase.class) {
                 if (INSTANCE == null) {
                     INSTANCE = Room.databaseBuilder(context.getApplicationContext(),
                                     AppDatabase.class, MyConstants.ROOMDB_DBNAME)
-                            // .fallbackToDestructiveMigration() // Uncomment this during development to auto-wipe DB if you change the model
+                            .fallbackToDestructiveMigration() // during development to auto-wipe DB if you change the model
                             .build();
                 }
             }
