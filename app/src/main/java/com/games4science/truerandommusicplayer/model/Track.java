590e0e7306bc65a16b8cc712ae79550d06120d93
PaulTamayoS <70984734+PaulTamayoS@users.noreply.github.com>
2026-05-04 16:15:10 +0200

    changed Id for uri as primary key

--- app/src/main/java/com/games4science/truerandommusicplayer/model/Track.java
+++ app/src/main/java/com/games4science/truerandommusicplayer/model/Track.java
@@ -4,26 +4,20 @@ import androidx.annotation.NonNull;
 import androidx.room.Entity;
 import androidx.room.PrimaryKey;
 
-@Entity(tableName = "tracks") // This tells Room to create a table
+@Entity(tableName = "tracks")
 public class Track {
 
-    @PrimaryKey(autoGenerate = true)
-    private int id; // Needed for the DB to uniquely identify rows
+    @PrimaryKey
+    @NonNull
+    private final String uriString; // Use the URI as the unique ID
 
     @NonNull
     private final String name;
-
-    @NonNull
-    private final String uriString; // Room prefers Strings over Uri objects
 
     public Track(@NonNull String name, @NonNull String uriString) {
         this.name = name;
         this.uriString = uriString;
     }
-
-    // Getters and Setters
-    public int getId() { return id; }
-    public void setId(int id) { this.id = id; }
 
     @NonNull
     public String getName() { return name; }
